e857ecf9726c804fb66eea35bbb9272be51b3e5e
Edouard Schlumberger <e.schlum@gmail.com>
2012-10-03 17:52:30 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mystaffin::Application.config.secret_token = '67d1a3722973988247c88afccb3ff971fcb87677024f2c21acd52c73c2b25daf4f29a4bc7577558db5ec18cc88fb81a02094695e98f6d58f8638ef2f25493032'
