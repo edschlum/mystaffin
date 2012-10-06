d70eb41e6e791cb6fe59964fc5a3927f6fcae313
Edouard Schlumberger <e.schlum@gmail.com>
2012-10-06 20:30:59 +0200

    allz

--- db/migrate/20121005140924_remove_class_from_statuts.rb
+++
@@ -1,9 +0,0 @@
-class RemoveClassFromStatuts < ActiveRecord::Migration
-  def up
-    remove_column :statuts, :class
-  end
-
-  def down
-    add_column :statuts, :class, :text
-  end
-end
--- db/migrate/20121005154553_remove_jour_from_dispos.rb
+++
@@ -1,9 +0,0 @@
-class RemoveJourFromDispos < ActiveRecord::Migration
-  def up
-    remove_column :dispos, :jour
-  end
-
-  def down
-    add_column :dispos, :jour, :date
-  end
-end
