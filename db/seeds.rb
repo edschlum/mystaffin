0b37b351942bcdaaa9b7f1e4e48f926f8085df61
Edouard Schlumberger <e.schlum@gmail.com>
2012-10-19 18:17:52 +0200

    preparing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,16 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 puts 'CREATING ROLES'
-#Role.create([
-#  { :name => 'admin' }, 
-#  { :name => 'user' }, 
-#  { :name => 'manager' }
-#], :without_protection => true)
-puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'Admin Admin', :email => 'e.schlum@gmail.com', :password => 'edouard', :password_confirmation => 'edouard'
-puts 'SETTING UP DEFAULT USER LOGIN'
-user1 = User.create! :name => 'First User', :email => 'user4@example.com', :password => 'please', :password_confirmation => 'please'
-puts 'SETTING UP DEFAULT USER LOGIN'
-user2 = User.create! :name => 'Third User', :email => 'user5@example.com', :password => 'please', :password_confirmation => 'please'
+Role.create([
+  { :name => 'admin' }, 
+  { :name => 'user' }, 
+  { :name => 'manager' }
+], :without_protection => true)
+#puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :name => 'Admin Admin', :email => 'edouard.schlumberger@gmail.com', :password => 'edouard', :password_confirmation => 'edouard'
+#puts 'SETTING UP DEFAULT USER LOGIN'
+#user1 = User.create! :name => 'First User', :email => 'user4@example.com', :password => 'please', :password_confirmation => 'please'
+#puts 'SETTING UP DEFAULT USER LOGIN'
+#user2 = User.create! :name => 'Third User', :email => 'user5@example.com', :password => 'please', :password_confirmation => 'please'
 user.add_role :admin
-user1.add_role :manager
+#user1.add_role :manager
