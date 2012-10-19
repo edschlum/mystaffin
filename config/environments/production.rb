51ee7a2272fdfcf58017b357585b7bbba234eaa9
Edouard Schlumberger <e.schlum@gmail.com>
2012-10-19 10:47:59 +0200

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,21 @@ Mystaffin::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'www.mystaff.in' }
+  config.action_mailer.delivery_method = :smtp
+  # change to false to prevent email from being sent during development
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["noreplymystaffin@gmail.com"],
+    password: ENV["mystaffin2012"]
+  }  
 end
