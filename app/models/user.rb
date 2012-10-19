eba542f1a1f696ab9fef6e1376abe6fb23cd8126
Edouard Schlumberger <e.schlum@gmail.com>
2012-10-19 16:07:12 +0200

    admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ rolify
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :first_name
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :first_name, :admin
   # attr_accessible :title, :body
   has_and_belongs_to_many :teams
   has_many :dispos
