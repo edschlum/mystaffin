3337cfa059df1fa61db5c7f41b21277d6e40d0a7
Edouard Schlumberger <e.schlum@gmail.com>
2012-10-19 11:41:11 +0200

    grr

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,7 @@ Mystaffin::Application.configure do
    ActionMailer::Base.delivery_method = :smtp   
    ActionMailer::Base.perform_deliveries = true   
    ActionMailer::Base.raise_delivery_errors = true   
-   ActionMailer::Base.smtp_settings = {   
-   :enable_starttls_auto => true,     
+   ActionMailer::Base.smtp_settings = {     
    :address            => 'smtp.gmail.com',   
    :port               => 587,   
    :domain             => 'mystaff.in',    
