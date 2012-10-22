2d3d1095a2a28a3e57f6c2cfb2ccd901db6f2827
Edouard Schlumberger <e.schlum@gmail.com>
2012-10-22 17:43:22 +0200

    confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121021171044) do
+ActiveRecord::Schema.define(:version => 20121022160500) do
 
   create_table "dispos", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -76,8 +76,12 @@ ActiveRecord::Schema.define(:version => 20121021171044) do
     t.string   "name"
     t.text     "first_name"
     t.boolean  "admin",                  :default => false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
